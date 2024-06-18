efebc46536f484c2eda4e2c0422cbb7f07f50329
lily1029 <lily.zhao1029@gmail.com>
2024-06-18 14:16:39 -0700

    fixed error

--- SearchInABigSortedArray.py
+++ SearchInABigSortedArray.py
@@ -1,3 +1,12 @@
+class ArrayReader:
+    def __init__(self, array):
+        self.array = array
+
+    def get(self, index):
+        if index >= len(self.array):
+            return float('inf')  # Simulate an out-of-bounds access with a large value
+        return self.array[index]
+
 class Solution:
     """
     @param reader: An instance of ArrayReader.
@@ -5,28 +14,29 @@ class Solution:
     @return: An integer which is the first index of target.
     """
     def searchBigSortedArray(self, reader, target):
-        # write your code here
-        start, end = 0, 1 
+        start, end = 0, 1
         
-        while  reader.get(end) <= target:
-            end = 2 * end 
-      
+        # Expand the search range exponentially until the end is greater than the target
+        while reader.get(end) < target:
+            end = 2 * end
+        
+        # Perform binary search within the determined range
         while start + 1 < end:
-            mid = (start + end) // 2 
+            mid = (start + end) // 2
             if reader.get(mid) < target:
-                start = mid 
+                start = mid
             else:
-                end = mid 
-        
+                end = mid
         
         if reader.get(start) == target:
             return start
         if reader.get(end) == target:
-            return end 
+            return end
         return -1
-if __name__ =='__main__':
+
+if __name__ == '__main__':
     ll = Solution()
-    reader = [1, 3, 6, 9, 21, ...]
+    reader = ArrayReader([1, 3, 6, 9, 21])
     target = 3
     x = ll.searchBigSortedArray(reader, target)
-    print(x)
+    print(x)  # Output should be 1
