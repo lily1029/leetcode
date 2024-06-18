e446402f47fd1e57487b372cf25edcede00fbbe3
lily1029 <lily.zhao1029@gmail.com>
2024-06-18 11:56:59 -0700

    ClassicalBinarySearch

---
+++ ClassicalBinarySearch.py
@@ -0,0 +1,33 @@
+class Solution:
+    """
+    @param nums: An integer array sorted in ascending order
+    @param target: An integer
+    @return: An integer
+    """
+    def findPosition(self, nums, target):
+        # write your code here
+        if not nums:
+            return -1
+        
+        start, end = 0, len(nums) - 1
+
+        while start + 1 < end:
+            mid = (start + end) // 2 
+            if nums[mid] < target:
+                start = mid
+            else:
+                end = mid
+        
+        if nums[start] == target:
+            return start
+        
+        if nums[end] == target:
+            return end 
+
+        return -1
+if __name__ =='__main__':
+    ll = Solution()
+    nums = [1, 2, 2, 4, 5, 5]
+    target = 2
+    x = ll.findPosition(nums, target)
+    print(x)
--- TopologicalSorting.py
+++ TopologicalSorting.py
@@ -1,8 +1,33 @@
 
-class DirectedGraphNode:
+import collections
+import networkx as nx
+#Definition for a Directed graph node
+
+class DirectedGraphNode1:
     def __init__(self, x):
         self.label = x
         self.neighbors = []
+
+class DirectedGraphNode2:
+    def __init__(self, x):
+        self.label = x
+        self.neighbors = []
+
+class DirectedGraphNode3:
+    def __init__(self, x):
+        self.label = x
+        self.neighbors = []
+
+class DirectedGraphNode4:
+    def __init__(self, x):
+        self.label = x
+        self.neighbors = []
+
+class DirectedGraphNode5:
+    def __init__(self, x):
+        self.label = x
+        self.neighbors = []
+
 
 
 
@@ -44,4 +69,36 @@ class Solution:
         return topo_order
 if __name__ == '__main__':
     # Create nodes
+    graph = nx.DiGraph()
+    graph.add_edges_from([("A", "B"), ("A", "D"), ("B", "D"), ("B", "C"), ("D", "E")])
+    print(graph.in_edges("E"))  # => [('a', 'e'), ('d', 'e')]
+
+    ll1 = DirectedGraphNode3(1)
+    ll1.label = 'C'
+    ll1.neighbors = []
+
+    ll2 = DirectedGraphNode5(2)
+    ll2.label = 'E'
+    ll2.neighbors = []
+
+    ll3 = DirectedGraphNode4(3)
+    ll3.label = 'D'
+    ll3.neighbors = [ll2]
+
+    ll4 = DirectedGraphNode2(4)
+    ll4.label = 'B'
+    ll4.neighbors = [ll1, ll3]
+
+
+    ll5 = DirectedGraphNode1(5)
+    ll5.label = 'A'
+    ll5.neighbors  = [ll4, ll3]
+    
+    solution = Solution()
+    x = solution.topSort(graph)
+    print(x)
+
+
+
+
     
