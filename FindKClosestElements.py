539258e1678cbb102e165976f1e0bfb0ac586c66
lily1029 <lily.zhao1029@gmail.com>
2024-06-19 10:40:21 -0700

    Fixed an error

--- FindKClosestElements.py
+++ FindKClosestElements.py
@@ -1,6 +1,4 @@
-from typing import (
-    List,
-)
+from typing import List
 
 class Solution:
     """
@@ -14,7 +12,7 @@ class Solution:
         right = self.find_upper_closest(a, target)
         left = right - 1
     
-        # 两根指针从中间往两边扩展，依次找到最接近的 k 个数
+        # Two pointers from the middle to both sides to find the k closest numbers
         results = []
         for _ in range(k):
             if self.is_left_closer(a, target, left, right):
@@ -26,15 +24,11 @@ class Solution:
         
         return results
     
-    def find_upper_closest(self, a, target):
+    def find_upper_closest(self, a: List[int], target: int) -> int:
         # find the first number >= target in A
         start, end = 0, len(a) - 1
         while start + 1 < end:
             mid = (start + end) // 2
-            # if a[mid] >= target:
-            #     end = mid
-            # else:
-            #     start = mid
 
             if a[mid] < target:
                 start = mid
@@ -47,20 +41,21 @@ class Solution:
         if a[end] >= target:            
             return end
         
-        # 找不到的情况
+        # If not found
         return end + 1
         
-    def is_left_closer(self, a, target, left, right):
+    def is_left_closer(self, a: List[int], target: int, left: int, right: int) -> bool:
         if left < 0:
             return False
         if right >= len(a):
             return True
         return target - a[left] <= a[right] - target
+
 if __name__ == '__main__':
     ll = Solution()
     a = [1, 2, 3]
     target = 2
     k = 3
-    x = ll.find_upper_closest(a, target, k)
+    # Call k_closest_numbers instead of find_upper_closest
+    x = ll.k_closest_numbers(a, target, k)
     print(x)
-    
