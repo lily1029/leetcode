161fb5c0e386d0d7fea0cf93b4dccde5171067ef
lily1029 <lily.zhao1029@gmail.com>
2024-06-21 13:35:09 -0700

    FlattenBinaryTreeToLinkedList

---
+++ FlattenBinaryTreeToLinkedList.py
@@ -0,0 +1,50 @@
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+def build_tree():
+    node_1 = TreeNode(1)
+    node_2 = TreeNode(2)
+    node_3 = TreeNode(3)
+   
+    node_1.left = node_2
+    node_1.right = node_3
+
+    return node_1
+
+
+class Solution:
+    """
+    @param root: a TreeNode, the root of the binary tree
+    @return: nothing
+    """
+    def flatten(self, root):
+        # write your code here
+        self.helper(root)
+    
+    def helper(self, root):
+        if not root:
+            return None
+        
+        left_last = self.helper(root.left)
+        right_last = self.helper(root.right)
+        
+        #connect
+        if left_last:
+            left_last.right = root.right
+            root.right = root.left 
+            root.left = None
+        
+        if right_last:
+            return right_last
+        if left_last:
+            return left_last
+        return root
+
+if __name__ == '__main__':
+    root = build_tree()
+    ll = Solution()
+    x = ll.flatten(root)
+    print(x)
