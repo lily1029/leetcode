58a0d3454fed7dc1993391fdb1af6159fb13023c
lily1029 <lily.zhao1029@gmail.com>
2024-06-26 16:31:40 -0700

    SumRootToLeafNumbers

---
+++ SumRootToLeafNumbers.py
@@ -0,0 +1,35 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def build_tree():
+    node_1 = TreeNode(1)
+    node_2 = TreeNode(2)
+    node_3 = TreeNode(3)
+
+    node_1.left = node_2
+    node_1.right = node_3
+
+    return node_1
+
+class Solution:
+    def sum_numbers(self, root: TreeNode) -> int:
+        def dfs(root: TreeNode, prevTotal: int) -> int:
+            if not root:
+                return 0
+            total = prevTotal * 10 + root.val
+            if not root.left and not root.right:
+                return total
+            else:
+                return dfs(root.left, total) + dfs(root.right, total)
+
+        return dfs(root, 0)
+
+if __name__ == '__main__':
+    root = build_tree()
+    ll = Solution()
+    x = ll.sum_numbers(root)
+
+    print(x)
