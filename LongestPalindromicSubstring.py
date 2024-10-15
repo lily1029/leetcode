494c5f20bcfc960504f801fd230d218dc3aecc88
lily1029 <lily.zhao1029@gmail.com>
2024-10-15 14:35:37 -0700

    Longest Palindromic Substring

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -8,21 +8,23 @@ class Solution:
         if not s:
             return ""
         
+        #这里找到sub string 中找到符合题意要求的string
         sub = ""
-        #define a result string 
+        #这里找最长的sub string
         longest = ""
 
-        #we go through the string to find the middle 
-        # in order to proceed the longest one
+        #这里的idea是一个一个字符切割，当是奇数string,由中间的向两边两个
+        #指针进行比较，如果是偶数长度string,左右两边进行比较
+        #所以这里要go through整个string一遍，time: O(n)
         for mid in range(len(s)):
-            # when the string is odd length
+            #odd string, 所以从中间开始阔，mid中间值一样
             sub = self.is_palindrom(s, mid, mid)
             #if find the sub string is longer 
             #than the global string (longest)
             if len(sub) > len(longest):
                 longest = sub
             
-            # when the string is even length
+            #even string,指针在中间，左右两边差1
             sub = self.is_palindrom(s, mid, mid + 1)
             if len(sub) > len(longest):
                 longest = sub
