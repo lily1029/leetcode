3c6038ca51fce519df08f87d6890f567a90b6264
lily1029 <lily.zhao1029@gmail.com>
2024-10-23 10:06:02 -0700

    Word Ladder

--- WordLadder.py
+++ WordLadder.py
@@ -9,22 +9,33 @@ class Solution:
     """
     def ladderLength(self, start, end, dict):
         # write your code here
+        #we put end in dict in order to compare with start
         dict.append(end)
 
+        #put the first word start in queue
         queue = collections.deque([start])
+        #as long as we put it in queue, we set it as a visited node
+        #we use a set to keep track
         visited = set([start])
 
+        #set variable as a distance,这里是所求的结果
         distance = 0 
 
+        #go through the queue, while queue is not empty 
         while queue:
             distance += 1
+            #go through the queue
             for i in range(len(queue)):
+                #pop out the word and compare it with end, 
+                #if equal, return distance
                 word = queue.popleft()
 
                 if word == end:
                     return distance
 
-
+                #if word != end, we look for the next words which is one bit different
+                #from the word, actually we list all the words and check whether it is 
+                #appear in dict or not 
                 for next_word in self.get_next_words(word, dict):
                     if next_word not in dict or next_word in visited:
                         continue 
@@ -32,7 +43,8 @@ class Solution:
                     visited.add(next_word)
         return 0 
 
-     
+    # O(26 * L^2)
+    # L is the length of word 
     def get_next_words(self, word, dict):
         words = []
 
