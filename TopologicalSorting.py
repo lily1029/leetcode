c96a57c4663af4ab110735fbcb077736f04e3852
lily1029 <lily.zhao1029@gmail.com>
2024-10-25 10:34:41 -0700

    Topological Sorting

--- TopologicalSorting.py
+++ TopologicalSorting.py
@@ -11,44 +11,72 @@ class Solution:
     @param: graph: A list of Directed graph node
     @return: Any topological order for the given graph.
     """
-    def get_indegree(self, graph):
-        indegrees = {node: 0 for node in graph}
-        for node in graph:
-            for neighbor in node.neighbors:
-                indegrees[neighbor] += 1
-        return indegrees
-
+    
     def topSort(self, graph):
-        indegrees = self.get_indegree(graph)
-        queue = collections.deque([node for node in graph if indegrees[node] == 0])
-        topo_order = []
-
+        # write your code here
+        node_to_indegree = self.get_indegree(graph)
+        # bfs begins here
+        # we use order to store the result topological sorting order
+        order = []
+        # we put all nodes which have in-degree = 0 here in start_nodes
+        start_nodes = [n for n in graph if node_to_indegree[n] == 0]
+        # put all start_nodes in queue
+        queue = collections.deque(start_nodes)
+        # go through the queue and pop out the first one and append to order list
         while queue:
             node = queue.popleft()
-            topo_order.append(node.label)
+            order.append(node)
+            # then we go through this node's all neighbours, because this node is               
+            # been poped out, so all this node 's neighbours' in-degree should - 1                  
             for neighbor in node.neighbors:
-                indegrees[neighbor] -= 1
-                if indegrees[neighbor] == 0:
+                node_to_indegree[neighbor] -= 1
+                # if this node 's neighbor node in-degree = 0, it should put it in queue            
+                if node_to_indegree[neighbor] == 0:
                     queue.append(neighbor)
-        return topo_order
+        return order 
+
+
+    def get_indegree(self, graph):
+        # we use a hashmap to record each node 's in-degree
+        # for x in graph , here x means each node in graph
+        # graph = [0, 1, 2, 3, 4, 5], after go through all nodess in graph
+        # {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+        node_to_indegree = {x: 0 for x in graph}
+
+        # we go through each node in the graph
+        for node in graph:
+            # we go through each node 's neighbours
+            for neighbor in node.neighbors:
+                # in the hashmap, the key is neighbor (node), if this node
+                # has a neighbor , this neighbor 's in-degree should + 1
+                node_to_indegree[neighbor] += 1
+
+        # after this above for loop, we can get all nodes' in-degree, and 
+        # we return  to the beginning in order to execute the rest code
+        return node_to_indegree
 
 if __name__ == '__main__':
     # Create nodes
-    nodeA = DirectedGraphNode('A')
-    nodeB = DirectedGraphNode('B')
-    nodeC = DirectedGraphNode('C')
-    nodeD = DirectedGraphNode('D')
-    nodeE = DirectedGraphNode('E')
+    node0 = DirectedGraphNode('0')
+    node1 = DirectedGraphNode('1')
+    node2 = DirectedGraphNode('2')
+    node3 = DirectedGraphNode('3')
+    node4 = DirectedGraphNode('4')
+    node5 = DirectedGraphNode('5')
 
     # Define neighbors
-    nodeA.neighbors = [nodeB, nodeD]
-    nodeB.neighbors = [nodeC, nodeD]
-    nodeD.neighbors = [nodeE]
+    node0.neighbors = [node1, node2, node3]
+    node1.neighbors = [node4]
+    node2.neighbors = [node4, node5]
+    node3.neighbors = [node4, node5]
 
     # Create graph
-    graph = [nodeA, nodeB, nodeC, nodeD, nodeE]
+    graph = [node0, node1, node2, node3, node4, node5]
 
     # Perform topological sort
     solution = Solution()
     topo_order = solution.topSort(graph)
     print(topo_order)
+
+
+
