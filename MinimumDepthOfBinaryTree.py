add252bccbe29dcbdea164421326e606bee41b65
lily1029 <lily.zhao1029@gmail.com>
2025-02-24 14:49:22 -0800

    Minimum Depth Of a Binary Search Tree

--- MinimumDepthOfBinaryTree.py
+++ MinimumDepthOfBinaryTree.py
@@ -26,11 +26,22 @@ class Solution:
     def minDepth(self, root):
         if root is None:
             return 0
+        #此题做法是:如果想知道the shortest path among all the paths 
+        #from root to leaf, 可以用divide and conquer method
+        #分别找出root下的左子树的shortest path, 在找出右子树的shortest path,
+        #然后挑出左右子树中最小的shortest path 在加上root这个节点，就是整棵树的
+        #shortest path
+
+        #divide parts
         leftDepth = self.minDepth(root.left)
         rightDepth = self.minDepth(root.right)
+
         # 当左子树或右子树为空时，最小深度取决于另一颗子树
         if leftDepth == 0 or rightDepth == 0:
             return leftDepth + rightDepth + 1
+        
+        #这里找出整棵树的最小值来自于左子树或是右子树，然后加上root 根节点就是整棵树的 shortest path
+        #conquer part
         return min(leftDepth, rightDepth) + 1
 if __name__ == '__main__':
     root = build_tree()
