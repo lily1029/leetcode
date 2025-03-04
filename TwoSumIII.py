8ed280ea6a2f1a4b02082e6e91a7b19709210c8d
lily1029 <lily.zhao1029@gmail.com>
2025-03-04 13:46:53 -0800

    Two Sum III - Data Structure Design

--- TwoSumIII.py
+++ TwoSumIII.py
@@ -1,27 +1,36 @@
-class TwoSum:
-    """
-    @param number: An integer
-    @return: nothing
-    """
-    def __init__(self):
-        self.counter = {}
-        
-    def add(self, number):
-        # write your code here
-        self.counter[number] = self.counter.get(number, 0) + 1 
+class TwoSum(object):
 
-    """
-    @param value: An integer
-    @return: Find if there exists any pair of numbers which sum is equal to the value.
-    """
-    def find(self, target):
-        # write your code here
-        for key in self.counter:
-            if target - key in self.counter and target != key * 2:
-                return True 
-            if target == key * 2 and self.counter[key] >= 2:
+    def __init__(self):
+        # initialize your data structure here
+        self.count = {}
+        
+    # Add the number to an internal data structure.
+    # @param number {int}
+    # @return nothing
+    def add(self, number):
+        if number in self.count:
+            self.count[number] += 1
+        else:
+            self.count[number] = 1
+
+    # Find if there exists any pair of numbers which sum is equal to the value.
+    # @param value {int}
+    # @return true if can be found or false
+    def find(self, value):
+        #go through the count,用value - num 看有没有差在count里，这里要注意是找pair,所以count[num] > 1
+        #可以有2个相同数字也算pair, 而且还有一个条件就是value - num != num，是说这个pair的数字不想等时
+        for num in self.count:
+            #当满足这个条件就返回true, 这里的find花费了O(n) time go through the count
+            if value - num in self.count and (value - num != num or self.count[num] > 1):
                 return True
+        
+        #走到这步说明没有这个pair, 返回false
         return False
+
+# Your TwoSum object will be instantiated and called as such:
+# twoSum = TwoSum()
+# twoSum.add(number)
+# twoSum.find(value)
 
 if __name__ == '__main__':
     ll = TwoSum()
@@ -29,7 +38,7 @@ if __name__ == '__main__':
     y = ll.add(3)
     z = ll.add(5)
     t = ll.add(5)
-    q = ll.find(4)
+    q = ll.find(10)
     w = ll.find(7)
     print(q)
     print(w)
