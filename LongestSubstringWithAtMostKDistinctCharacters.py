ae00c0832efb3e66cac781ea61417fd070c26b5c
lily1029 <lily.zhao1029@gmail.com>
2025-03-17 14:28:33 -0700

    Longest Substring With At Most K Distince Characters

--- LongestSubstringWithAtMostKDistinctCharacters.py
+++ LongestSubstringWithAtMostKDistinctCharacters.py
@@ -1,14 +1,23 @@
 class Solution:
-  
+    
+    #同向双指针，此题的做法就是：一次遍历即可，遍历的同时记录
+    #每个字母出现的次数和当前子串中不同字母的长度。其中定义一个
+    #左指针记录左边界，当前的子串即左边界到遍历位置对应的子串长度
+    #如果不同字母的个数超过k,则向右调整左边界（左指针向右移),
+    #遍历同时维护最长子串的长度即可
     def lengthOfLongestSubstringKDistinct(self, s, k):
+
         if not s:
             return 0 
         
+        #左指针
         left = 0 
+        #全局变量，算最终结果，最长的k distinct characters
         longest = 0 
+        #用counter算每个字母出现过几次，
         counter = {}
         
-        # use right pointer to go through list s 
+        #这里是右指针 to go through list s 
         for right in range(len(s)):
             # here is to count how many characters appear in s 
             counter[s[right]] = counter.get(s[right], 0) + 1            
@@ -16,6 +25,9 @@ class Solution:
             # 这里要判断如果超过了k个字符后，要移动左指针
             # 判断条件是left <= right and 超过k个字符了
             while left <= right and len(counter) > k:
+                #这里因为超过了k,所以调整左指针，去掉一个左边的字母，如果归0，
+                #就delete 掉左边的字母，从下个子串算维持 k个distinct chars
+                #所以left + 1 
                 counter[s[left]] -= 1 
                 if counter[s[left]] == 0:
                     del counter[s[left]]
--- MoveZeroes.py
+++ MoveZeroes.py
@@ -41,3 +41,6 @@ if __name__ == '__main__':
     ll.moveZeroes(nums)
     print(nums)
 
+
+
+
