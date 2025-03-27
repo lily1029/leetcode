12c38fc3b6fb687ef37423d6a16cdc901a5a7f67
lily1029 <lily.zhao1029@gmail.com>
2025-03-27 11:52:41 -0700

    Binary Tree Path Sum IV

--- BinaryTreePathSumIV.py
+++ BinaryTreePathSumIV.py
@@ -13,7 +13,8 @@ class Solution:
         #self.ans是最后的结果
         self.ans = 0
         #因为nums里的数字是ascending, 所以可以通过最后一个数//100算出树的深度
-        #这里树的深度其实是矩阵的行数，如果一个三位数想得到百位数，整出100得百位数字
+        #题目中hundreds digit代表树的深度
+        #这里树的深度其实是矩阵的行数，如果一个三位数想得到百位数，整除100得百位数字
         # 221 // 100 = 2， 树的深度为2，放入的矩阵，矩阵的行数为2
         #self.mx 是矩阵的行数，这个例子，行数为2
         self.mx = nums[n - 1] // 100
@@ -42,10 +43,10 @@ class Solution:
             # Extract depth and position
             #算行位置row position
             dep = nums[i] // 100
-            #算列position
+            #算列position，也就是这个第二位的数ten digit在矩阵中的index
             pos = nums[i] // 10 % 10
             
-            # Place the value in the grid
+            # Place the third digit value in the grid
             #因为grid是从g[0][0]开始，所以要减一
             #想得到一个数的最后一位 数字%10 e.g nums[i]%10
             g[dep - 1][pos - 1] = nums[i] % 10
