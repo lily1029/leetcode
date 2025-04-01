b8b8a0d05798900bddce9f3ba8fa7c7ebf854772
lily1029 <lily.zhao1029@gmail.com>
2025-04-01 15:05:38 -0700

    Subsets

--- Subsets.py
+++ Subsets.py
@@ -22,13 +22,19 @@ class Solution:
         res.append(subset[:])
 
         # 为subset新增一位元素
+        #这里，如果k=i时，表示可以向子集中添加nums[i:]
+        #中任何一个元素，此时面临len(nums) - i个选择
+        #这里要for 循环len(nums)的长度，因为会有
+        #不同的 nums[i]开头的子集，
         for i in range(k, len(nums)):
+            
+            #这里表示以nums[i]开头的子集
             subset.append(nums[i])
 
-            # 这里进行下一层搜索
+            # 这里是对nums[i]开头的子集，进行下一层搜索
             self.dfs(nums, i + 1, subset, res)
-            
-            # 回溯
+
+            # 当搜索结束，回溯
             del subset[-1]
 if __name__ == '__main__':
     ll = Solution()
