3e3a16d46704cd813f6d257c8bf9e065ad75d7ba
lily1029 <lily.zhao1029@gmail.com>
2025-04-02 22:58:40 -0700

    Sudoku Solver

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -6,15 +6,21 @@ class Solution:
     @return: nothing
     """
     def solveSudoku(self, board):
-
+        
+        #backtrack() 是 dfs 算法
         self.backtrack(board, 0, 0)
 
+        #返回深搜完的board
         return board
     
+    #这里是dfs算法，i: 代表行 j：代表列
     def backtrack(self, board, i, j):
+        # m, n 代表棋盘有9行，和 9 列
         m, n = 9, 9
 
         # 到达第n列，越界，换到下一行第0列重新开始
+        # 当 j 列 == 9 时，说明这一行已经结束，到下一行
+        #重新从0列开始放值, 所以 i + 1 （行 + 1）
         if j == n:
             return self.backtrack(board, i + 1, 0)
         
@@ -29,15 +35,17 @@ class Solution:
         #从board的(0,0)开始从1 开始试直到9
         for val in range(1, 10):
 
-            # 如果遇到不合法的数字，就跳过
+            #如果遇到不合法的数字，就跳过
+            #这里判断val的值在board[i][j]是否valid
+            #valid主要看行，列，9宫格里，有没有重复的，
             if not self.isValid(board, i, j, val):
                 continue
 
-            # 经过验证，可以确定的一个数字
+            # 经过验证，可以确定/放的一个数字
             board[i][j] = val
             
             # 如果找到一个可行解，立即结束
-            # 找完上一个后，找同行的下一个点j+1 
+            # 找完上一个后，找同行的下一个列，所以 j+1 
             # 这一行,列(i, j+1)都满足条件下返回True 
             if self.backtrack(board, i, j + 1):
                 return True
@@ -49,7 +57,7 @@ class Solution:
         return False
     
     def isValid(self, board, row, col, val):
-
+        #这个游戏是放1-9的数字，所以for循环 range(9)
         for i in range(9):
             # 判断行是否存在重复
             if board[row][i] == val:
@@ -80,4 +88,5 @@ if __name__ == '__main__':
     x = ll.solveSudoku(board)
     print(x)
             
-            
+
+
