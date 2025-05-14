4845d19165a2d84eb7341a4697821895fffaf203
lily1029 <lily.zhao1029@gmail.com>
2025-05-14 15:22:45 -0700

    Merge K Sorted Arrays

--- MergeKSortedArrays.py
+++ MergeKSortedArrays.py
@@ -5,24 +5,31 @@ class Solution:
     @return: a sorted array
     """
     def mergekSortedArrays(self, arrays):
-        #result存merge后的结果
+        #result 里存 merge 后的结果
         result = []
+        
+        #这里用heap 去sort
         heap = []
         
         #for循环题目给的arrays
         for index, array in enumerate(arrays):
+
             if len(array) == 0:
                 continue
             
             #并将循环的第一个元素用一个三元组推到堆里
+            #index是array里的次序，0:是每个小array里的index
             heapq.heappush(heap, (array[0], index, 0))
         
         #当heap的长度不为零时，对它进行循环
         while len(heap):
+            
             #取出heap中的最小值
             val, x, y = heap[0]
+
             #把这个值pop()出来
             heapq.heappop(heap)
+
             #并且把这个值放入到result中
             result.append(val)
             
@@ -31,6 +38,7 @@ class Solution:
             #行坐标和列坐标+1 
             if y + 1 < len(arrays[x]):
                 heapq.heappush(heap, (arrays[x][y + 1], x, y + 1))
+
         return result
 if __name__ == '__main__':
     # Example usage:
