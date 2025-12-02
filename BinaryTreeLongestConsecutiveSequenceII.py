d853f24531c0f427dd4d08f51457fc5fd3bec847
lily1029 <lily.zhao1029@gmail.com>
2025-12-02 08:16:45 -0800

    Updated the comments at the last line

--- BinaryTreeLongestConsecutiveSequenceII.py
+++ BinaryTreeLongestConsecutiveSequenceII.py
@@ -36,16 +36,16 @@ class Solution:
         return self.length
     
     def search(self, root):
-        #当root为空时， 返回此root的最大上升长度up_length 为0
-        #这里up_length是root的左子树的最大上升长度
-        #返回此root的 最大下降长度down_length 为0
-        #这里down_length是root的右子树的最大下降长度
+        #当root为空时， 返回此root的最大上升长度leftUp_length 为0
+        #这里leftUp_length是root的左子树的最大上升长度
+        #返回此root的 最大下降长度rightDown_length 为0
+        #这里rightDown_length是root的右子树的最大下降长度
         if not root:
             return [0, 0]
         
-        #对每一个节点，它的 up_length 和 down_length 初始值为1
-        up_length = 1
-        down_length = 1
+        #对每一个节点，它的 leftUp_length 和 rightDown_length 初始值为1
+        leftUp_length = 1
+        rightDown_length = 1
 
         #divide part to left subtree
         left = self.search(root.left)
@@ -54,25 +54,25 @@ class Solution:
         
         #在这里判断是不是可以找到一条合法路径
         if root.left:
-            #加上头节点可以是合法路径，up_length + 1
+            #加上头节点可以是合法路径，leftUp_length + 1
             if root.left.val - 1 == root.val:
-                up_length = max(up_length, left[0] + 1)
-            #这里是可以找到一条合法的down_length 路径
+                leftUp_length = max(leftUp_length, left[0] + 1)
+            #这里是可以找到一条合法的rightDown_length 路径
             if root.left.val + 1 == root.val:
-                down_length = max(down_length, left[1] + 1)
+                rightDown_length = max(rightDown_length, left[1] + 1)
         
         #这里属于镜像操作，上面看root.left, 这里查看root.right做一样的操作
         if root.right:
             if root.right.val - 1 == root.val:
-                up_length = max(up_length, right[0] + 1)
+                leftUp_length = max(leftUp_length, right[0] + 1)
             if root.right.val + 1 == root.val:
-                down_length = max(down_length, right[1] + 1)
+                rightDown_length = max(rightDown_length, right[1] + 1)
         
-        #更新全局变量最大值，包括加上root的值（up_length + down_length - 1)
-        self.length = max(self.length, up_length + down_length - 1)
+        #更新全局变量最大值，包括加上root的值（leftUp_length + rightDown_length - 1)
+        self.length = max(self.length, leftUp_length + rightDown_length - 1)
         
-        #这里返回每个节点的最大上升长度up_length和最大下降长度down_length
-        return [up_length, down_length]
+        #这里返回每个节点的左子树最大上升长度leftUp_length和右子树最大下降长度rightDown_length
+        return [leftUp_length, rightDown_length]
 if __name__ == '__main__':
     root = build_tree()
     ll = Solution()
